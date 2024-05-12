b6bc11646f6e3a5a6f405b59799cb25530ff25b3
Tuchechka <nazarzaharchuk000@gmail.com>
2024-05-12 05:33:08 +0300

    create registration

--- client/src/components/registration-organization.jsx
+++ client/src/components/registration-organization.jsx
@@ -1,6 +1,16 @@
-function RegistrationOrganization() {
+import "../css/registration-organization.css"
+
+function RegistrationOrganization({ handleEmailChange, handleNicknameChange, handlePasswordChange }) {
     return (
-        <></>
+        <>
+        <input type="email" placeholder="Електронна пошта" onChange={handleEmailChange} className="reg-org-input" required></input>
+        <input type="text" placeholder="Назва рятувальної служби/організації" onChange={handleNicknameChange} className="reg-org-input" required></input>
+        <label className="reg-org-lable">
+            Документи для верифікації
+            <input type="file" id="reg-org-doc" multiple ></input>
+        </label>
+        <input type="password" placeholder="Пароль" onChange={handlePasswordChange} className="reg-org-input" required></input>
+        </>
     )
 }
 
--- client/src/pages/registration.jsx
+++ client/src/pages/registration.jsx
@@ -1,6 +1,84 @@
+import "../css/registration.css"
+import logoOnPage from "../assets/logo-on-page.png"
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import RegistrationOrganization from "../components/registration-organization"
+import RegistrationVolunteer from "../components/registration-volunteer"
+
 function Registration() {
+    const [comp, setComp] = useState('RegistrationVolunteer');
+    const [buttonOrg, setButtonOrg] = useState('buttonOther');
+    const [buttonVol, setButtonVol] = useState('buttonChois');
+    const [nickname, setNickname] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [canAdd, setCanAdd] = useState(false);
+    const navigate = useNavigate();
+  
+    const handleClick = () => {
+        navigate('/auth'); 
+    };
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+        {comp === 'RegistrationOrganization' ? setCanAdd(true) : setCanAdd(false)}
+    };
+
+    const handleNicknameChange = (e) => {
+        setNickname(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const changeToVol = () => {
+        setComp('RegistrationVolunteer');
+        setButtonOrg('buttonOther');
+        setButtonVol('buttonChois');
+    };
+
+    const changeToOrg = () => {
+        setComp('RegistrationOrganization');
+        setButtonOrg('buttonChois');
+        setButtonVol('buttonOther');
+    };
+
     return (
-        <></>
+        <div className="registration-body">
+            <div className="registration-div-left"></div>
+            <div className="registration-div-center">
+                <div className="registration-form-top">
+                    <img src={logoOnPage} alt="" />
+                    <p>ЗАРЕЄСТРУВАТИСЯ</p>
+                    <div className="registration-div-change">
+                        <button onClick={changeToOrg} className={buttonOrg}>Організація</button>
+                        <button onClick={changeToVol} className={buttonVol}>Волонтер</button>
+                    </div>
+                    <form>
+                        {comp === 'RegistrationOrganization' ? (
+                            <RegistrationOrganization
+                                handleEmailChange={handleEmailChange}
+                                handleNicknameChange={handleNicknameChange}
+                                handlePasswordChange={handlePasswordChange}
+                            />
+                        ) : (
+                            <RegistrationVolunteer
+                            handleEmailChange={handleEmailChange}
+                            handleNicknameChange={handleNicknameChange}
+                            handlePasswordChange={handlePasswordChange}
+                        />
+                        )}
+                        <button className="registration-button-submit">Зареєструватись</button>
+                    </form>
+                </div>
+                <div className="registration-form-botom">
+                    <p>Вже маєте аккаунт?</p>
+                    <button onClick={handleClick}>Увійти</button>
+                </div>
+            </div>
+            <div className="registration-div-right"></div>
+        </div>
     )
 }
 
