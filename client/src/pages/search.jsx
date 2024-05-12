846d9976d7c29ff60d40e22895b44954412f6782
yurkenchik <142983974+yurkenchik@users.noreply.github.com>
2024-05-12 12:59:43 +0300

    connected backend auth apis

--- client/src/pages/search.jsx
+++ client/src/pages/search.jsx
@@ -1,14 +1,28 @@
+import { useEffect, useState } from "react";
 import Header from "../components/header";
 import "../css/search.css"
+import { getAnnouncements } from "../api/searchApi";
 
 function Search() {
     let announcement = [
         {id: 1, title: 'Іванин Іван Іванович', description: 'Пропала людина віком 30 років, 180 сантиметрів зросту', loseDate: '12.09.2023', lastSeeLocation: 'Львів', foundUser: ' Знайдено', imageUrl: ''},
         {id: 2, title: 'Іванин Іван Іванович', description: 'Пропала людина віком 30 років, 180 сантиметрів зросту', loseDate: '12.09.2023', lastSeeLocation: 'Київ', foundUser: ' В пошуках', imageUrl: ''},
-        {id: 3, title: 'Іванин Іван Іванович', description: 'Пропала людина віком 30 років, 180 сантиметрів зросту', loseDate: '12.09.2023', lastSeeLocation: 'Львів', foundUser: ' Не знайдено', imageUrl: ''},
+        {id: 3, title: 'Іванин Іван Іванович', description: 'Пропала людина віком 30 років, 180 сантиметрів зросту', loseDate: '12.09.2023', lastSeeLocation: 'Львів', foundUser: ' Не знайдено', imageUrl: 'w'},
         {id: 4, title: 'Іванин Іван Іванович', description: 'Пропала людина віком 30 років, 180 сантиметрів зросту', loseDate: '12.09.2023', lastSeeLocation: 'Вроцлав', foundUser: ' Не знайдено', imageUrl: ''},
         {id: 5, title: 'Іванин Іван Іванович', description: 'Пропала людина віком 30 років, 180 сантиметрів зросту;', loseDate: '12.09.2023', lastSeeLocation: 'Львів', foundUser: ' Не знайдено', imageUrl: ''}
     ];
+
+    const [announcements, setAnnouncements] = useState([])
+
+    useEffect(() => {
+        const fetchAnnouncements = async () => {
+            const fetchResult = await getAnnouncements()
+            setAnnouncements(fetchAnnouncements)
+        }
+
+        fetchAnnouncements()
+    }, [])
+
     return (
         <>
         <Header />
@@ -20,17 +34,17 @@ function Search() {
                 </div>
             </div>
             <div className="search-right-75">
-                {announcement.map((req, index) => (
-                    <div key={index} className="search-div-request">
+                {Array.isArray(announcements) && announcements.map((req, index) => (
+                    <div key={req.id} className="search-div-request">
                         <div className="search-image-found">
                             <img src={req.imageUrl}/>
-                            <p id="search-found">{req.foundUser}</p>
+                            <p id="search-found">{req.found}</p>
                         </div>
                         <div className="search-title-description">
                             <p id="search-title">{req.title}</p>
                             <p id="search-description">{req.description}</p>
                             <div className="search-data-location">
-                                <p>{req.lastSeeLocation}</p>
+                                <p>{req.lastSeenLocation}</p>
                                 <p>{req.loseDate}</p>
                             </div>
                         </div>
